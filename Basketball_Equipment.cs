6870db1d3bc49f7e00efcf53e8be791db60ddd18
Lordofshape <77344949+Lordofshape@users.noreply.github.com>
2021-10-21 22:17:24 +0300

    Conditional Statements

---
+++ Basketball_Equipment.cs
@@ -0,0 +1,22 @@
+﻿namespace Basketball_Equipment
+{
+using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int yearTax = int.Parse(Console.ReadLine());
+            //Logic
+            double sneakers = yearTax * 0.6;
+            double outfit = sneakers * 0.8;
+            double ball = outfit * 0.25;
+            double accessories = ball * 0.20;
+            double needenSum = yearTax + sneakers + outfit + ball + accessories;
+
+            Console.WriteLine(needenSum);
+
+        }
+    }
+}
---
+++ Guess_Password.cs
@@ -0,0 +1,21 @@
+﻿namespace Guess_Password
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string enterpasword = Console.ReadLine();
+            string password = "s3cr3t!P@ssw0rd";
+            if (password==enterpasword)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
