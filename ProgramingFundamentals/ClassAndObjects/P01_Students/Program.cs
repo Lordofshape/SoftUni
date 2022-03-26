4e1e2fedb7bdae030f1a45cb3a4f0c0c4224c671
Lordofshapes <medko3699@gmail.com>
2022-03-26 22:07:50 +0200

    Update

---
+++ ProgramingFundamentals/ClassAndObjects/P01_Students/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_Students
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            int numberOFStudents = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOFStudents; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string firstName = input[0];
+                string lastName = input[1];
+                double grade = double.Parse(input[2]);
+                Student student = new Student(firstName, lastName, grade);
+                students.Add(student);
+            }
+            List<Student> orderedStudents = new List<Student>(students.OrderByDescending(s => s.Grade));
+            foreach (var student in orderedStudents)
+            {
+                Console.WriteLine(student);
+            }
+
+        }
+    }
+    public class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+          
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}: {this.Grade:f2}";
+        }
+        
+    }
+}
---
+++ ProgramingFundamentals/StringProcessingLab/P01_Diagonal_Differance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_Diagonal_Differance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rowCol = int.Parse(Console.ReadLine());
+            var matrix = new int[rowCol, rowCol];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] inputRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i,j] = inputRow[j];
+                }
+            }
+            int leftSum = 0;
+            int rightSum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                leftSum+=matrix[i,i];
+                rightSum+=matrix[i, matrix.GetLength(1) - 1-i];
+            }
+            Console.WriteLine(Math.Abs(leftSum-rightSum));
+        }
+    }
+}
