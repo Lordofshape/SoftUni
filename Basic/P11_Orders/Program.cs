091ebd65c48242c15d7875bf648846978fb517a5
Medikko <mehmeddukov@gmail.com>
2022-03-28 21:42:47 +0300

    Update

---
+++ Basic/P11_Orders/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P11_Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal totalCoffeePrice = 0;
+            for (int i = 0; i < n; i++)
+            {
+                decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsuleCount = int.Parse(Console.ReadLine());
+                decimal priceOfCoffee = days * capsuleCount * pricePerCapsule;
+                totalCoffeePrice += priceOfCoffee;
+                Console.WriteLine($"The price for the coffee is: ${priceOfCoffee:f2}");
+            }
+            Console.WriteLine($"Total: ${totalCoffeePrice:f2}");
+        }
+    }
+}
